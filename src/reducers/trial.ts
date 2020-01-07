afce7491fa61c1adabb4968b9c5ef8d5623ad166
Zach Parks <zach@alliware.com>
2020-01-06 19:51:05 -0600

    completed trial reducer

---
+++ src/reducers/trial.ts
@@ -0,0 +1,54 @@
+import Trial from "../structs/trial";
+import {
+    ACQUIT_VOTE,
+    END_TRIAL_ACQUITTAL,
+    END_TRIAL_LYNCHED,
+    LYNCH_VOTE,
+    RESET_TRIAL_STATE,
+    START_TRIAL,
+    TrialActions,
+    TrialPlayerAction,
+} from "../interfaces/trial-actions";
+import produce from "immer";
+
+export function trialReducer(state: Trial = new Trial(), action: TrialActions): Trial {
+    return produce(state, draft => {
+        switch (action.type) {
+            case RESET_TRIAL_STATE:
+                return new Trial();
+
+            case START_TRIAL:
+                draft.accused = (action as TrialPlayerAction).player;
+                draft.active = true;
+                break;
+
+            case END_TRIAL_ACQUITTAL:
+                draft.lynchVotes = [];
+                draft.acquitVotes = [];
+
+                if (draft.accused !== null) {
+                    draft.immune.push(draft.accused);
+                } else {
+                    throw new Error(
+                        "Attempted to push null accused to immune list. Are you sure there's a trial going?",
+                    );
+                }
+
+                draft.accused = null;
+                draft.active = false;
+                break;
+
+            case END_TRIAL_LYNCHED:
+                draft.active = false;
+                draft.startable = false;
+                break;
+
+            case LYNCH_VOTE:
+                draft.lynchVotes.push((action as TrialPlayerAction).player);
+                break;
+            case ACQUIT_VOTE:
+                draft.acquitVotes.push((action as TrialPlayerAction).player);
+                break;
+        }
+    });
+}
--- src/test/reducers/trial.test.ts
+++ src/test/reducers/trial.test.ts
@@ -1,10 +1,89 @@
-it.todo("should create an initial trial object on store creation");
+import { createStore } from "redux";
+import Trial from "../../structs/trial";
+import { trialReducer } from "../../reducers/trial";
+import {
+    acquitVote,
+    endTrialAcquittal,
+    endTrialLynched,
+    lynchVote,
+    resetTrialState,
+    startTrial,
+} from "../../actions/trial";
+import { createTestPlayer } from "../fixtures/player";
+
+let store: ReturnType<typeof createStore>;
+beforeEach(() => {
+    store = createStore(trialReducer);
+});
+
+it("should create an initial trial object on store creation", () => {
+    expect(store.getState()).toBeInstanceOf(Trial);
+});
 
 describe("Action handlers", () => {
-    it.todo("when RESET_TRIAL_STATE is fired, replace state with new trial object");
-    it.todo("when START_TRIAL is fired, set accused player and active");
-    it.todo("when END_TRIAL_ACQUITTAL is fired, reset votes, disable active, and add player to immune list");
-    it.todo("when END_TRIAL_LYNCHED is fired, set startable to false and end trial active");
-    it.todo("when LYNCH_VOTE is fired, add player to lynch votes array");
-    it.todo("when ACQUIT_VOTE is fired, add player to acquit votes array");
+    it("when START_TRIAL is fired, set accused player and active", () => {
+        const player = createTestPlayer();
+        store.dispatch(startTrial(player));
+
+        const state = store.getState() as Trial;
+        expect(state.accused).toBe(player);
+        expect(state.active).toBe(true);
+    });
+    it("when END_TRIAL_ACQUITTAL is fired, reset votes, disable active, and add player to immune list", () => {
+        const player = createTestPlayer();
+
+        store.dispatch(startTrial(player));
+        store.dispatch(endTrialAcquittal());
+
+        const state = store.getState() as Trial;
+        expect(state.lynchVotes.length).toBe(0);
+        expect(state.acquitVotes.length).toBe(0);
+        expect(state.active).toBe(false);
+        expect(state.accused).toBe(null);
+        expect(state.immune[0]).toBe(player);
+    });
+    it("when END_TRIAL_LYNCHED is fired, set startable to false and end trial active", () => {
+        const player = createTestPlayer();
+
+        store.dispatch(startTrial(player));
+        store.dispatch(endTrialLynched());
+
+        const state = store.getState() as Trial;
+        expect(state.active).toBe(false);
+        expect(state.startable).toBe(false);
+    });
+    it("throw an error if END_TRIAL_ACQUITTAL is called if trial not active", () => {
+        expect(() => store.dispatch(endTrialAcquittal())).toThrow();
+    });
+    it("when LYNCH_VOTE is fired, add player to lynch votes array", () => {
+        const player = createTestPlayer();
+
+        store.dispatch(lynchVote(player));
+
+        const state = store.getState() as Trial;
+        expect(state.lynchVotes[0]).toBe(player);
+        expect(state.lynchVotes.length).toBe(1);
+    });
+    it("when ACQUIT_VOTE is fired, add player to acquit votes array", () => {
+        const player = createTestPlayer();
+
+        store.dispatch(acquitVote(player));
+
+        const state = store.getState() as Trial;
+        expect(state.acquitVotes[0]).toBe(player);
+        expect(state.acquitVotes.length).toBe(1);
+    });
+    it("when RESET_TRIAL_STATE is fired, replace state with new trial object", () => {
+        const player = createTestPlayer();
+
+        store.dispatch(startTrial(player));
+        store.dispatch(lynchVote(player));
+        store.dispatch(acquitVote(player));
+        store.dispatch(endTrialLynched());
+
+        store.dispatch(resetTrialState());
+
+        const state = store.getState() as Trial;
+        expect(state).toStrictEqual(new Trial());
+    });
 });
